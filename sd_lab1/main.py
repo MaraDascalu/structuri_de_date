6d291621b4754c793a96442104e3ab00358b4959
MaraDascalu <dascalu.mara.2001@gmail.com>
2021-03-12 11:00:18 +0200

    tema_sortari

--- sd_lab1/main.py
+++ sd_lab1/main.py
@@ -1,17 +1,44 @@
-import random, sys, time
+import random, sys, time, copy
 sys.setrecursionlimit(10**6)
+
+def testSort(a, v):
+    dict_init = {}
+    dict_fin = {}
+    for elem in a:
+        if elem in dict_init:
+            dict_init[elem] += 1
+        else:
+            dict_init[elem] = 1
+
+    for i in range (len(v) - 1):
+        if v[i] > v[i+1]:
+            return 0
+
+    for elem in v:
+        if elem in dict_fin:
+            dict_fin[elem] += 1
+        else:
+            dict_fin[elem] = 1
+
+    if dict_fin == dict_init:
+        return True
+    return False
+
 
 #
 # Quicksort random pivot
 #
 
 def quicksort_rp (v, start, stop ):
-    if start < stop:
-        p = random.randrange(start, stop)
-        v[start], v[p] = v[p], v[start]
-        pivot = partitionare(v, start, stop)
-        quicksort_rp(v,start, pivot-1)
-        quicksort_rp(v,pivot+1, stop)
+    if len(v)> 5000:
+        return False
+    else:
+        if start < stop:
+            p = random.randrange(start, stop)
+            v[start], v[p] = v[p], v[start]
+            pivot = partitionare(v, start, stop)
+            quicksort_rp(v,start, pivot-1)
+            quicksort_rp(v,pivot+1, stop)
 
 def partitionare (v,start, stop):
     pivot = start
@@ -25,11 +52,14 @@ def partitionare (v,start, stop):
 #
 
 def quicksort (v, start, stop ):
-    if start < stop:
-        part_rand(v, start, stop)
-        pivot = partitionare (v, start, stop)
-        quicksort(v,start, pivot-1)
-        quicksort(v,pivot+1, stop)
+    if len(v) > 5000:
+        return False
+    else:
+        if start < stop:
+            part_rand(v, start, stop)
+            pivot = partitionare (v, start, stop)
+            quicksort(v,start, pivot-1)
+            quicksort(v,pivot+1, stop)
 
 def part_rand(v, start, stop):
     pivot_rand = (start+stop) // 2
@@ -52,60 +82,69 @@ def heapify(v, n, idx):
         heapify(v, n, parent)
 
 def heap_sort(v):
-    for i in range(len(v)//2 - 1):
-        heapify(v, len(v), i)
+    if len(v) > 500000:
+        return False
+    else:
+        for i in range(len(v)//2 - 1, -1, -1):
+            heapify(v, len(v), i)
 
-    for i in range(len(v) - 1, 0, -1):
-        v[0], v[i] = v[i], v[0]
-        heapify(v, i, 0)
+        for i in range(len(v) - 1, 0, -1):
+            v[0], v[i] = v[i], v[0]
+            heapify(v, i, 0)
 
 #
 # Cocktail sort
 #
 
 def cocktail_sort(v):
-    swap = True
-    start = 0
-    end = len(v) - 1
-    while swap == True:
-        swap = False
-        for i in range(start, end):
-            if v[i] > v[i+1]:
-                v[i], v[i+1] = v[i+1], v[i]
-                swap = True
-        if swap == False:
-            break
-        swap = False
-        end -= 1
-        for i in range (end - 1, start - 1, -1):
-            if v[i] > v[i+1]:
-                v[i], v[i+1] = v[i+1], v[i]
-                swap = True
-        start += 1
+    if len(v)> 5000:
+        return False
+    else:
+        swap = True
+        start = 0
+        end = len(v) - 1
+        while swap == True:
+            swap = False
+            for i in range(start, end):
+                if v[i] > v[i+1]:
+                    v[i], v[i+1] = v[i+1], v[i]
+                    swap = True
+            if swap == False:
+                break
+            swap = False
+            end -= 1
+            for i in range (end - 1, start - 1, -1):
+                if v[i] > v[i+1]:
+                    v[i], v[i+1] = v[i+1], v[i]
+                    swap = True
+            start += 1
 
 #
 # Counting sort
 #
 
 def counting_sort(v):
-    elem_max = max(v)
-    elem_min = int(min(v))
-    range_elem = elem_max - elem_min + 1
-    cs = [0] * range_elem
-    output = [0] * len(v)
+    if max(v) > 10000000 or len(v) > 10000000:
+        return False
+    else:
+        elem_max = max(v)
+        elem_min = int(min(v))
+        range_elem = elem_max - elem_min + 1
+        cs = [0] * range_elem
+        output = [0] * len(v)
 
-    for i in range (len(v)):
-        cs[v[i] - elem_min] += 1
+        for i in range (len(v)):
+            cs[v[i] - elem_min] += 1
 
-    for i in range (1,len(cs)):
-        cs[i] += cs[i - 1]
+        for i in range (1,len(cs)):
+            cs[i] += cs[i - 1]
 
-    for i in range (len(v)-1, -1, -1):
-        output[cs[v[i] - elem_min] -1] = v[i]
-        cs[v[i] - elem_min] -= 1
+        for i in range (len(v)-1, -1, -1):
+            output[cs[v[i] - elem_min] -1] = v[i]
+            cs[v[i] - elem_min] -= 1
 
-    for i in range(len(v)):
-        v[i] = output[i]
+        for i in range(len(v)):
+            v[i] = output[i]
 
 #
 # Radix sort
@@ -129,38 +168,78 @@ def counting_sort_r(v, exp):
         v[i] = output[i]
 
 def radix_sort(v):
-    elem_max = max(v)
-    exp = 1
-    while elem_max // exp >0:
-        counting_sort_r(v, exp)
-        exp *= 10
+    if len(v) >= 1000000:
+        return False
+    else:
+        elem_max = max(v)
+        exp = 1
+        while elem_max // exp >0:
+            counting_sort_r(v, exp)
+            exp *= 10
 
-v = random.sample(range(1000000),1000)
-t0 = time.time()
-quicksort(v,0, len(v) - 1)
-t1 = time.time()
-print("Qiucksort time : ",t1 - t0)
-t0 = time.time()
-quicksort_rp(v, 0, len(v) - 1)
-t1 = time.time()
-print("Quicksort random pivot time : ", t1 - t0)
-t0 = time.time()
-heap_sort(v)
-t1 = time.time()
-print("Heap sort time : ", t1 - t0)
-t0 = time.time()
-cocktail_sort(v)
-t1 = time.time()
-print("Cocktil sort time : ", t1 - t0)
-t0 = time.time()
-counting_sort(v)
-t1 = time.time()
-print("Counting sort time : ", t1 - t0)
-t0 = time.time()
-radix_sort(v)
-t1 = time.time()
-print("Radix sort time : ", t1 - t0)
-t0 = time.time()
-v_sort = sorted(v)
-t1 = time.time()
-print("Python sort time : ", t1 - t0)
+with open("input.txt") as f:
+    tests = f.read()
+
+tests = [int(x) for x in tests.split()]
+nr_tests = tests.pop(0)
+for i in range(0, 2*nr_tests - 1, 2):
+    print("Test nr", i//2 + 1)
+    n = tests[i]
+    nr_max = tests[i+1]
+    print(n, nr_max)
+    a = [random.randint(0, nr_max) for _ in range(n)]
+    v = copy.deepcopy(a)
+    t0 = time.time()
+    result = quicksort(v, 0, len(v) - 1)
+    if result == False:
+        print("QuickSort merge prea incet.")
+    else:
+        t1 = time.time()
+        print("Quicksort time : ", t1 - t0, testSort(a, v))
+    v = copy.deepcopy(a)
+    t0 = time.time()
+    result = quicksort_rp(v, 0, len(v) - 1)
+    if result == False:
+        print("QuickSort (random pivot) merge prea incet.")
+    else:
+        t1 = time.time()
+        print("Quicksort (random pivot) time : ", t1 - t0, testSort(a, v))
+    v = copy.deepcopy(a)
+    t0 = time.time()
+    result = heap_sort(v)
+    if result == False:
+        print("HeapSort merge prea incet.")
+    else:
+        t1 = time.time()
+        print("HeapSort time : ", t1 - t0, testSort(a, v))
+    v = copy.deepcopy(a)
+    t0 = time.time()
+    result = cocktail_sort(v)
+    if result == False:
+        print("CocktailSort merge prea incet.")
+    else:
+        t1 = time.time()
+        print("CocktailSort time : ", t1 - t0, testSort(a, v))
+    v = copy.deepcopy(a)
+    t0 = time.time()
+    result = counting_sort(v)
+    if result == False:
+        print("CountingSort merge prea incet.")
+    else:
+        t1 = time.time()
+        print("CountingSort time : ", t1 - t0, testSort(a, v))
+    v = copy.deepcopy(a)
+    t0 = time.time()
+    result = radix_sort(v)
+    if result == False:
+        print("RadixSort merge prea incet.")
+    else:
+        t1 = time.time()
+        print("RadixSort time : ", t1 - t0, testSort(a, v))
+    v = copy.deepcopy(a)
+    t0 = time.time()
+    v = sorted(v)
+    t1 = time.time()
+    print("PythonSort time : ", t1 - t0, testSort(a, v))
+    print("\n")
+
---
+++ sd_lab2_2/sd_lab2_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sd_lab2_2
+//
+//  Created by Mara Dascalu on 10/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
